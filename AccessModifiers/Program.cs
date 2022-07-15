5529aa087b09138be20a67556893543202f9856a
Sricharan <ayibasirs@gmail.com>
2022-07-15 16:00:08 +0530

    Examples for Access Modifiers

---
+++ AccessModifiers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Dog
+    {
+        public void name()
+        {
+            Console.WriteLine("Sam");
+        }
+        private void color()
+        {
+            Console.WriteLine("White");
+        }
+        protected void age()
+        {
+            Console.WriteLine("5 Years");
+        }
+    }
+
+    class Animal
+    {
+        static void Main(string[] args)
+        {
+            Dog labrador = new Dog();
+
+            labrador.name();
+            labrador.color();
+            labrador.age();
+        }
+    }
+}
